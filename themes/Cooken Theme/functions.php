f541cd6aff8e45f77dcdafbd627c523d8605ce3b
imdat99 <imdat2999@gmail.com>
2021-08-15 22:42:18 +0700

    add bootstrap

--- themes/Cooken Theme/functions.php
+++ themes/Cooken Theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function cooken_files() {
+    wp_enqueue_style( 'bootstrap@5.1.0','//cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css' );
     wp_register_style( 'cooken_main_styles', get_stylesheet_uri() );
     wp_enqueue_style( 'cooken_main_styles');
 }
