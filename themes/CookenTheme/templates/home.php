11582044ca280c748ae32ea6750df609c5669c3c
tiendat6299 <tiendat621999@gmail.com>
2021-08-19 11:24:43 +0700

    Header_to_Project

---
+++ themes/CookenTheme/templates/home.php
@@ -0,0 +1,132 @@
+<?php get_header(); 
+
+/*
+    Template Name: Home
+*/
+?>
+
+<div class="main">
+    <div class="container">     
+        <div class="section" id="banner">
+            <div class="section_banner_parent">
+                <div class="section_banner">
+                    <div class="section_banner_content">
+                        <div class="row banner">
+                            <div class="col-6">
+                                <div class="title_banner bounceInLeft">
+                                    <h1>
+                                        Cooken<br>
+                                        Trading
+                                    </h1>
+                                    <span>
+                                        Nâng tầm giá trị cuộc sống – Mang những dịch vụ tốt nhất tới gia đình bạn
+                                    </span>
+                                    <br>
+                                    <a href="#">
+                                        <span>
+                                            <button>TÌM HIỂU THÊM</button>
+                                        </span> 
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="img_banner bounceInRight ">
+                                    <div class="img_banner_child">
+                                        <img src="http://localhost/Cooken/wp-content/uploads/2021/08/Group-504-1.png" alt="banner">
+                                    </div>  
+                                </div>
+                            </div>
+                        </div>
+                    </div>                
+                </div>
+            </div>         
+        </div>
+        <div class="section" id="our_service">
+            <div class="title_section">
+                <h2>
+                Dịch Vụ Của Chúng Tôi
+                </h2>
+            </div>
+            <div  class="row">
+                <div class="col col-sm-6 col_custom_section">
+                    <div class="row1" style="width: 13.3vw;height: 27.6vw;background: #DF5E44;z-index:1; position: absolute;    position: absolute;margin-left: -5vw;margin-top: 4vw;"></div>
+                    <div class="hover-image" style="position:relative">
+									<a href="https://cookentrading.com.vn/thuong-mai/"><img class="img_custom_section" src="https://cookentrading.com.vn/wp-content/uploads/2021/05/Screenshot-2021-05-26-214126.png" width="100%" height="100%" style="z-index: 2; position:relative  " alt="Thương mại"></a>
+									
+                    </div>
+                
+                </div>
+            <div class="col col-sm-6 col_custom_section">
+                <div class="row1-text">
+                    <h3>Thương Mại</h3>
+                    <p>Cooken Trading cung cấp đa dạng các loại sản phẩm trên nhiều lĩnh vực như: làm đẹp, thực phẩm và chăm sóc sức khoẻ.</p>
+									<a href="https://cookentrading.com.vn/thuong-mai/"><button>Xem thêm</button></a>
+                    </div>
+            </div>
+            </div>
+            <div style="margin-top: 1vw;" class="row">
+					    <div id="6299" class="col col-sm-6 col_custom_section image_thuongmai_front">
+                <div class="row2" style="width: 13.3vw;height: 35.7vw;background: #DF5E44;z-index:1;position: absolute;margin-left: 21vw;"></div>
+                <div class="hover-image" style="position:relative;margin-top: 7vw;margin-left: 4vw;">
+									<a href="https://cookentrading.com.vn/nhap-khau/"> <img  class="img_custom_section" src="https://cookentrading.com.vn/wp-content/uploads/2021/07/WhatsApp-Image-2021-07-09-at-14.22.33-1.jpeg" width="100%" height="100%" 	alt="Nhập khẩu"		                             style="z-index: 2;    position: relative;"></a>
+                </div>
+								</div>
+            <div class="col col-sm-6 col_custom_section">
+                <div class="row2-text">
+                    <h3>Nhập Khẩu</h3>
+                    <p>Cooken Trading trực tiếp nhập khẩu và nhận uỷ thác nhập khẩu các mặt hàng thực phẩm, mỹ phẩm và thực phẩm chức năng của nhiều nước trên thế giới.</p>
+									<a href="https://cookentrading.com.vn/nhap-khau/"><button>Xem thêm</button></a>
+                    </div>
+            </div>
+            <div id="0312" class="col col-sm-6 col_custom_section image__thuongmai_back">
+                <div class="row2" style="width: 13.3vw;height: 35.7vw;background: #DF5E44;z-index:1;position: absolute;margin-left: 16vw;"></div>
+                <div class="hover-image" style="position:relative;margin-top: 7vw;margin-left: 1vw;">
+									<a href="https://cookentrading.com.vn/nhap-khau/"> <img  class="img_custom_section" src="https://cookentrading.com.vn/wp-content/uploads/2021/07/WhatsApp-Image-2021-07-09-at-14.22.33-1.jpeg" width="100%" height="100%" style="z-index: 2;    position: relative;" alt="Nhập khẩu"></a>
+                                    </div>
+								</div>
+					    </div>
+					
+       
+            <div  class="row">
+            <div class="col col-sm-6 col_custom_section">
+                <div class="row3" style="width: 13.3vw;height: 25.6vw;;background: #DF5E44;z-index:1;position: absolute;margin-left: -4vw;margin-top: 1vw;"></div>
+                <div class="hover-image" style="position:relative;margin-top: 8vw;">
+                    
+									<a href="https://cookentrading.com.vn/xuat-khau/"> <img class="img_custom_section"  src="https://cookentrading.com.vn/wp-content/uploads/2021/07/WhatsApp-Image-2021-07-09-at-14.22.33.jpeg" width="100%" height="100%" style="z-index: 2;    position: relative;" alt="Xuất khẩu"></a>
+									
+            </div>
+                </div>
+             <div class="col col-sm-6 col_custom_section">
+                <div class="row3-text">
+                    <h3>Xuất Khẩu</h3>
+                    <p>Cooken Trading là đơn vị chuyên xuất khẩu các mặt hàng nông sản, thuỷ hải sản trong nước sang các thị trường tiềm năng trên toàn thế giới.</p>
+									<a href="https://cookentrading.com.vn/xuat-khau/"><button>Xem thêm</button></a>
+                    </div>
+             </div>
+            </div>
+        </div>
+        <div class="section" id="our_project">
+            <div class="title_section">
+                    <h2>
+                    Dự Án
+                    </h2>
+            </div>
+            <div class="owl-carousel owl-theme">
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-3.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-7.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-3.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-7.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-3.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-7.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-3.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-7.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-3.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-7.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-3.png"></div>
+                <div class="item"><img src="http://localhost/Cooken/wp-content/uploads/2021/08/anh-7.png"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer() ?>
